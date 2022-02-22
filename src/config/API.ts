5713fd640a73ebf280a6e98eff636b0a9ef6295f
sis <sis@HERCULES>
2022-02-22 12:56:04 +0300

    add app config files

---
+++ src/config/API.ts
@@ -0,0 +1,5 @@
+export enum API {
+    categories = 'https://teststrapitest.herokuapp.com/categories',
+    products = 'https://teststrapitest.herokuapp.com/products',
+    promoted = 'https://teststrapitest.herokuapp.com/promoted-categories',
+}
