9a6bce63786dd358fa5e101be0b519f953a62fc5
sis <sis@HERCULES>
2022-02-24 09:33:47 +0300

    function throttle

---
+++ src/core/utils/throttle.ts
@@ -0,0 +1,22 @@
+export function throttle(func: Function, ms: number) {
+    let isThrottled: boolean = false
+    let savedArgs: any
+    let savedThis: any
+
+    function wrapper(this: any) {
+        if (isThrottled) {
+            savedArgs = arguments
+            savedThis = this
+            return
+        }
+
+        func.apply(this, arguments)
+        isThrottled = true
+
+        setTimeout(() => {
+            isThrottled = false
+            savedArgs && wrapper.apply(savedThis, savedArgs)
+            savedThis = savedArgs = null
+        }, ms)
+    }
+}
