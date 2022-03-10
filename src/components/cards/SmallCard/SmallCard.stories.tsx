377b4f9c8af7ebaf1897f26c47629431b06869f1
sis <sis@HERCULES>
2022-03-10 11:22:49 +0300

    SmallCard

---
+++ src/components/cards/SmallCard/SmallCard.stories.tsx
@@ -0,0 +1,70 @@
+import React from 'react'
+import { ComponentStory, ComponentMeta } from '@storybook/react'
+import { SmallCard } from './SmallCard'
+
+export default {
+    title: 'SmallCard',
+    component: SmallCard,
+} as ComponentMeta<typeof SmallCard>
+
+const defaultArgs = {
+    product: {
+        id: 15,
+        name: 'HP - Spectre x360 2-in-1 13.5"',
+        price: 2065,
+        inStock: 6,
+        code: 'SKU 48646879',
+        description:
+            'HP - Spectre x360 2-in-1 13.5" 3K2K OLED Touchscreen Laptop - Intel Evo Core i7 - 16GB Memory - 1TB SSD + 32GB Intel Optane - Nightfall Black Tablet Notebook 14-ea1023dx',
+        color: 'black',
+        image: [
+            {
+                id: 22,
+                imageUrl: 'https://i.onthe.io/smngoz4hj4egou8ue.91d5f7a1.jpg',
+                imageAlt: 'HP - Spectre x360 2-in-1 13.5"',
+            },
+        ],
+        specs: [
+            {
+                id: 50,
+                name: 'CPU',
+                value: 'N/A',
+            },
+            {
+                id: 49,
+                name: 'CPU',
+                value: 'N/A',
+            },
+            {
+                id: 48,
+                name: 'CPU',
+                value: 'N/A',
+            },
+        ],
+        details: [],
+        rating: {
+            id: 14,
+            one: 0,
+            two: 0,
+            three: 0,
+            four: 0,
+            five: 0,
+            reviews: 0,
+        },
+        category: {
+            id: 38,
+            name: 'HP Spectre',
+        },
+        brand: {
+            id: 3,
+            name: 'Hewlett-Packard',
+        },
+    },
+}
+
+const Template: ComponentStory<typeof SmallCard> = (args) => (
+    <SmallCard {...defaultArgs} {...args} />
+)
+
+export const inStock = Template.bind({})
+inStock.args = {}
---
+++ src/components/cards/SmallCard/SmallCard.tsx
@@ -0,0 +1,62 @@
+import Button from 'components/buttons/Button'
+import Availability from 'components/parts/Availability'
+import Circle from 'components/parts/Circle'
+import Icon from 'components/parts/Icon'
+import Rating from 'components/parts/Rating'
+import React from 'react'
+import { ProductType } from 'types/ProductType'
+import styles from './SmallCard.module.less'
+
+type PropsType = {
+    product: ProductType
+    discount?: number
+}
+
+export const SmallCard: React.FC<PropsType> = ({ product, discount = 0 }) => {
+    const rating = [
+        product.rating.one,
+        product.rating.two,
+        product.rating.three,
+        product.rating.four,
+        product.rating.five,
+    ].filter((star) => star > 0)
+
+    const stars =
+        rating.length > 0 ? rating.reduce((acc, current) => acc + current) / rating.length : 0
+
+    const croppedDescription =
+        product.description.length > 60
+            ? `${product.description.substring(0, 60)}...`
+            : product.description
+
+    const oldPrice = (product.price - product.price * (discount / 100)).toFixed(2)
+    const newPrice = product.price.toFixed(2)
+
+    return (
+        <div className={styles.smallcard}>
+            <div className={styles.availability}>
+                <Availability count={product.inStock} />
+            </div>
+            <img src={product.image[0].imageUrl} alt={product.image[0].imageAlt} />
+            <div className={styles.circles}>
+                <Circle type='heart' callback={() => {}} />
+                <Circle type='stats' callback={() => {}} />
+            </div>
+            <div className={styles.stats}>
+                <Rating stars={stars} />
+                <div className={styles.reviews}>Reviews({product.rating.reviews})</div>
+            </div>
+            <div className={styles.name}>{croppedDescription}</div>
+            <div className={styles.price}>
+                <div className={styles.price__old}>${oldPrice}</div>
+                <div className={styles.price__new}>${newPrice}</div>
+            </div>
+            <div className={styles.cart}>
+                <Button preset='tranparent-blue'>
+                    <Icon name='cart' />
+                    &nbsp; Add To Cart
+                </Button>
+            </div>
+        </div>
+    )
+}
