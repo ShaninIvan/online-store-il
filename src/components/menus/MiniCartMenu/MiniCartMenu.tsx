dc4bb2980bc680681fdf25be0575d5b09f1d5084
sis <sis@HERCULES>
2022-03-23 13:37:08 +0300

    fixes

--- src/components/menus/MiniCartMenu/MiniCartMenu.tsx
+++ src/components/menus/MiniCartMenu/MiniCartMenu.tsx
@@ -1,18 +1,21 @@
 import Button from 'components/buttons/Button'
 import Icon from 'components/parts/Icon'
+import getPath from 'core/routing/getPath'
 import useAppSelector from 'hooks/useAppSelector'
 import useCartFunctions from 'hooks/useCartFunctions'
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 import styles from './MiniCartMenu.module.less'
 
 export const MiniCartMenu: React.FC = () => {
     const { orders } = useAppSelector((state) => state.cart)
 
     const { removeFromCart } = useCartFunctions()
+    const navigate = useNavigate()
 
     if (orders.length === 0)
         return (
-            <div className={styles.minicartmenu}>
+            <div className={`${styles.minicartmenu} ${styles.empty}`}>
                 <h3>My Cart</h3>
                 <div>Your cart is empty</div>
             </div>
@@ -24,6 +27,10 @@ export const MiniCartMenu: React.FC = () => {
 
     const deleteClickHandler = (id: number) => {
         removeFromCart(id)
+    }
+
+    const editClickHandler = () => {
+        navigate(getPath('/cart'))
     }
 
     return (
@@ -49,7 +56,7 @@ export const MiniCartMenu: React.FC = () => {
                             >
                                 <Icon name='close' />
                             </div>
-                            <div className={styles.control}>
+                            <div className={styles.control} onClick={() => editClickHandler()}>
                                 <Icon name='edit' />
                             </div>
                         </div>
