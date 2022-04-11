13efe9df9b956192ffc4f3ca8b0a82cc4594a479
sis <sis@HERCULES>
2022-04-11 10:59:14 +0300

    tests for Button

---
+++ src/components/buttons/Button/Button.test.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import Button from '.'
+
+describe('Button', () => {
+    test('Это кнопка', () => {
+        render(<Button preset='blue-white'>test</Button>)
+        expect(screen.getByRole('button')).toBeInTheDocument()
+    })
+    test('Текст кнопки виден', () => {
+        render(<Button preset='blue-white'>test</Button>)
+        expect(screen.getByText(/test/)).toBeInTheDocument()
+    })
+    test('При свойстве paypal = true добавляется картинка', () => {
+        render(
+            <Button preset='blue-white' paypal>
+                test
+            </Button>
+        )
+        expect(screen.getByRole('img')).toBeInTheDocument()
+    })
+    test('При свойстве paypal = false картинки нет', () => {
+        render(<Button preset='blue-white'>test</Button>)
+        expect(screen.queryByRole('img')).not.toBeInTheDocument()
+    })
+})
